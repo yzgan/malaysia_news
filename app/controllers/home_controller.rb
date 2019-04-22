f51600d531531cef6367aaac32e7149a87993cb6
Gan Yi Zhong <ganyizhong@gmail.com>
2019-04-22 12:39:24 +0800

    add creadit and year in footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :set_year, only: %i[index]
+
   def index
     @articles = Article.order(created_at: :desc).page(params[:page])
   end
@@ -6,4 +8,10 @@ class HomeController < ApplicationController
   def top_headlines
     render json: NewsApiServices.new.top_headlines
   end
+
+  private
+
+    def set_year
+      @year = Date.current.year
+    end
 end
