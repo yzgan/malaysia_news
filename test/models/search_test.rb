f75922fa9b2b4f60a60ed98a74406e94c2c2af5a
Gan Yi Zhong <ganyizhong@gmail.com>
2019-04-26 09:57:17 +0800

    Feature/resource search test (#4)
    * resource model test
    
    * search model test

--- test/models/search_test.rb
+++ test/models/search_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class SearchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @search_build = build :search
+  end
+
+  should define_enum_for(:status).with(%i[error ok])
+
+  context 'associations' do
+    should have_many(:articles).dependent(:nullify)
+  end
+
+  test 'valid search' do
+    assert @search_build.valid?
+  end
 end
