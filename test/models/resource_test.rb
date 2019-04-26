253113f287451dc02a1b1defe0ae8dc2bc3ea282
Gan Yi Zhong <ganyizhong@gmail.com>
2019-04-26 09:43:54 +0800

    resource model test

--- test/models/resource_test.rb
+++ test/models/resource_test.rb
@@ -4,4 +4,20 @@ class ResourceTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @resource_build = build :resource
+  end
+
+  context 'associations' do
+    should have_many(:articles).dependent(:nullify)
+  end
+
+  context 'validations' do
+    should validate_uniqueness_of :name
+  end
+
+  test 'valid article' do
+    assert @resource_build.valid?
+  end
 end
