31f44cf0005c58859aed533539ab04d84394cab0
Gan Yi Zhong <ganyizhong@gmail.com>
2019-04-29 12:02:00 +0800

    rails admin integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'home#index'
   resources :resources
   resources :articles, only: %i[index show edit update destroy]
