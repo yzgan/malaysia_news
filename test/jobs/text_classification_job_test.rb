d86abe2bebcbc1cacab355c21c1323a40e014bfe
Gan Yi Zhong <ganyizhong@gmail.com>
2019-07-11 11:37:03 +0800

    add test for text classification job

--- test/jobs/text_classification_job_test.rb
+++ test/jobs/text_classification_job_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
+require 'minitest/mock'
 
 class TextClassificationJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @article = create :article
+    @mock = Minitest::Mock.new
+  end
+
+  test 'enqueue jobs after article created' do
+    assert_enqueued_jobs 1
+    create :article
+    assert_enqueued_jobs 2
+  end
+
+  test "job result in execution classify article content" do
+    dummy_text = 'hello Keanu Reeves, You are breathtaking'
+    sample_data = {
+      categories: [
+        { name: '/actor', confidence: 0.6200000047683716 },
+        { name: '/Gamer/hype', confidence: 0.5799999833106995 }
+      ]
+    }
+    response_mock = Minitest::Mock.new
+    response_mock.expect(:to_h, sample_data)
+    @mock.expect(:classify, response_mock, [dummy_text])
+
+    assert_performed_jobs 1, only: TextClassificationJob do
+      Google::GoogleCloudLanguage.stub(:new, @mock) do
+        @new_article = create :article, content: dummy_text
+      end
+      @mock.verify
+    end
+    assert_equal sample_data.dig(:categories, 0, :name), @new_article.reload.category
+  end
 end
