76478a01e3bbd2a7984228c11efa38f512b3307f
Gan Yi Zhong <ganyizhong@gmail.com>
2019-07-19 10:24:08 +0800

    Bug/fix classification validation (#18)
    * remove article error in classification text
    
    * discard job on google retry error

--- app/interactions/classify_text.rb
+++ app/interactions/classify_text.rb
@@ -1,10 +1,6 @@
 class ClassifyText < ActiveInteraction::Base
   record :article
 
-  set_callback :execute, :before do
-    errors.merge!(article.errors) unless article.valid?
-  end
-
   def execute
     return article_classification_error unless valid_words_count?
     return unless Rails.application.config.external_application_services
