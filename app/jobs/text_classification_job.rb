6e4308f4fc0067a27cb78d92f0a183a1f948e530
yzgan <ganyizhong@gmail.com>
2019-07-19 09:56:55 +0800

    discard job on google retry error

--- app/jobs/text_classification_job.rb
+++ app/jobs/text_classification_job.rb
@@ -1,6 +1,8 @@
 class TextClassificationJob < ApplicationJob
   queue_as :default
 
+  discard_on Google::Gax::RetryError
+
   def perform(article)
     response = Google::GoogleCloudLanguage.new.classify article.content
     category = response.to_h.dig(:categories, 0, :name)
